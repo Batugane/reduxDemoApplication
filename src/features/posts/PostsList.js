c3f1ca2bc7bffe8392b97af4416298f154a098d7
batuhan duras <batugane@gmail.com>
2024-01-31 11:54:21 +0000

    feat: cached data refreshing

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,6 +7,7 @@ import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 
 import { useGetPostsQuery } from '../api/apiSlice'
+import classnames from 'classnames'
 
 let PostExcerpt = ({ post }) => {
   return (
@@ -30,9 +31,11 @@ export const PostsList = () => {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
+    refetch,
   } = useGetPostsQuery()
 
   const sortedPosts = useMemo(() => {
@@ -46,9 +49,15 @@ export const PostsList = () => {
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ))
+
+    const containerClassname = classnames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
@@ -56,6 +65,7 @@ export const PostsList = () => {
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   )
